542d32b9ccd5c879ca85e7dff3e8906d748ffba7
Martín Cigorraga <394089+cig0@users.noreply.github.com>
2026-04-08 23:38:28 -0300

    Refactor comments in pkgs-candidates.nix
    Removed comment about the set of packages for all hosts.

--- config/shortcuts/pkgs-candidates.nix
+++ config/shortcuts/pkgs-candidates.nix
@@ -1,10 +1,7 @@
 # @MODULON_SKIP
 /*
-  Set of packages for all hosts
   This file is extracted from packages.nix to improve modularity and maintainability.
-
-  Try-before-you-buy module (!). Make sure the corresponding option is enabled in the host's
-  profile module.
+  Try-before-you-buy module (!).
 */
 {
   hostKind,
